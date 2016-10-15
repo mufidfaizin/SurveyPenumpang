47c0ec21bb1ae88f6a2e7f00fce5471a16e50391
mufidzfaizin <muhammad_alfaizin_23rpl@student.smktelkom-mlg.sch.id>
2016-10-15 17:56:26 +0700

    void proses

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4021/surveikepuasanpenumpang/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4021/surveikepuasanpenumpang/MainActivity.java
@@ -67,6 +67,70 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         spJurusan.setSelection(0);
     }
 
+    private void proses()
+    {
+        if(isValid())
+        {
+            if(Pilih == 0 )
+            {
+                cbPilih = "Tidak mendapatkan";
+            }
+            else if(Pilih == 1 )
+            {
+                cbPilih = "Mendapatkan 1";
+            }
+            else if(Pilih == 2 )
+            {
+                cbPilih = "Mendapatkan 2";
+            }
+            else if(Pilih == 3 )
+            {
+                cbPilih = "Mendapatkan semua";
+            }
+            String comen = etComment.getText().toString();
+            String irb;
+
+            if (rgKelas.getCheckedRadioButtonId()!= -1)
+            {
+                RadioButton rb = (RadioButton) findViewById(rgKelas.getCheckedRadioButtonId());
+                irb = rb.getText().toString();
+            }
+            else
+            {
+                irb = null;
+            }
+
+            if (irb == null)
+            {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                builder1.setMessage("Anda belum mengisi input kepuasan!!");
+                builder1.setCancelable(true);
+
+                builder1.setNeutralButton(
+                        "Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                                //startActivity(new Intent(CourseActivity.this, MainActivity.class));
+                            }
+                        });
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+            else
+            {
+                tvHasil.setText("- Anda menggunakan bus Gunung Harta Jurusan "+ spJurusan.getSelectedItem().toString()+"\n" +
+                        "- Anda "+ cbPilih +" Pelayanan dari para crew\n- Anda menggunakan bus kelas: "+ irb+" \n" +
+                        "- Saran dan Kritik: " + comen+" ");
+
+                awal();
+            }
+        }
+    }
+
+
+
+
 
 
 
